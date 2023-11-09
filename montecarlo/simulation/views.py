cc7c06a0cd796f14fa1c16f43d7b899a2e1ede48
Adrian Croitor <adicroitor86@yahoo.com>
2023-11-09 17:41:11 +0200

    Added views for montecarlo based on django forms

--- montecarlo/simulation/views.py
+++ montecarlo/simulation/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from .forms import MonteCarloForm
+from .monte_carlo import CoinGeckoMonteCarloSimulation
 
+def montecarlo(request):
+    results = []
 
-def index(request):
-    return render(request, "simulation/index.html")
+    if request.method == 'POST':
+        form = MonteCarloForm(request.POST)
+        
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            coin_id = cleaned_data['coin_id']
+            years = cleaned_data['years']
+            principal_amount = cleaned_data['principal_amount']
+            investment_horizon = cleaned_data['investment_horizon']
+            num_simulations = cleaned_data['num_simulations']
+
+            monte_carlo = CoinGeckoMonteCarloSimulation(
+                coin_id, years, principal_amount, investment_horizon, num_simulations)
+            results = monte_carlo.run_simulation_and_get_results()
+                
+        
+    else:
+        
+        form = MonteCarloForm()
+        
+
+    return render(request, 'simulation/montecarlo.html', {'form': form, 'results': results})
