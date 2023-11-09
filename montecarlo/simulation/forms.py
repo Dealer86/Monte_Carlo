81db24ed8076a8d256efe35308c2e1ed81d8b098
Adrian Croitor <adicroitor86@yahoo.com>
2023-11-09 17:38:19 +0200

    Added form with fields for coin gecko monte carlo simulation class

---
+++ montecarlo/simulation/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class MonteCarloForm(forms.Form):
+    coin_id = forms.CharField(max_length=50, label='Cryptocurrency Symbol')
+    years = forms.IntegerField(label='Years of Historical Data')
+    principal_amount = forms.FloatField(label='Initial Principal Amount')
+    investment_horizon = forms.IntegerField(label='Investment Horizon (Years)')
+    num_simulations = forms.IntegerField(label='Number of Monte Carlo Simulations')
