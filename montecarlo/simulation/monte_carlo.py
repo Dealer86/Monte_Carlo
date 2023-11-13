112ed4d887b8a12b28b5d2513c7312fb91bceaa3
Adrian Croitor <adicroitor86@yahoo.com>
2023-11-13 19:08:23 +0200

    Added return_timestamps bool argument for fetch_price_data, used it in visualize_history_graph that generates a html fig.

--- montecarlo/simulation/monte_carlo.py
+++ montecarlo/simulation/monte_carlo.py
@@ -7,14 +7,15 @@ import mpld3
 
 
 class CoinGeckoMonteCarloSimulation:
-    def __init__(self, coin_id: str, years_of_price_data_to_collect_starting_from_current_year: int, principal_amount: float, investment_horizon: int, num_simulations: int ):
+    def __init__(self, coin_id: str, years_of_price_data_to_collect_starting_from_current_year: int, principal_amount: float, investment_horizon: int, num_simulations: int):
         self.coin_id = coin_id
         self.years = years_of_price_data_to_collect_starting_from_current_year
         self.principal_amount = principal_amount
         self.investment_horizon = investment_horizon
         self.num_simulations = num_simulations
+        
 
-    def fetch_price_data(self, coin_id, years):
+    def fetch_price_data(self, coin_id, years, return_timestamps: bool = False):
         """
         Fetches historical cryptocurrency price data from CoinGecko API.
 
@@ -42,7 +43,10 @@ class CoinGeckoMonteCarloSimulation:
                 timestamp_in_milliseconds = [e[0] for e in data["prices"]]
                 timestamps = [datetime.fromtimestamp(ts / 1000) for ts in timestamp_in_milliseconds]
                 # print(timestamps)
-                return prices
+                if not return_timestamps:
+                    return prices
+                else:
+                    return timestamps, prices
             else:
                 print(f"Failed to fetch data. Status code: {response.status_code}")
                 return None
@@ -52,6 +56,7 @@ class CoinGeckoMonteCarloSimulation:
         except (KeyError, ValueError) as e:
             print(f"Error while processing data: {e}")
             return None
+
 
     def calculate_log_returns(self, prices):
         """
@@ -76,10 +81,9 @@ class CoinGeckoMonteCarloSimulation:
         return returns
 
     
-
     def run_simulation(self):
         """
-        Runs simulations and returns the results as a string.
+        Runs simulations and returns the results.
         """
         prices = self.fetch_price_data(self.coin_id, self.years)
         if prices is not None:
@@ -90,23 +94,6 @@ class CoinGeckoMonteCarloSimulation:
 
         else:
             print(f"Failed to run simulation")
-            return []
-
-    def get_simulations_results(self):
-        results = []
-        image_path = 'static/simulation/monte_carlo.png'
-        if self.run_simulation():
-            for i, average_future_value in enumerate(self.run_simulation()):
-                results.append(f"Average future values of the investment for Monte Carlo Simulation {i + 1}: {average_future_value:.2f}$")
-
-            results.append(f"Total average of {self.num_simulations} Monte Carlo simulations: {np.mean(self.run_simulation()):.2f}$")
-
-            # self.visualize_simulation_results(total_average_simulations)
-            # result = '\n'.join(results)
-            
-            return results, image_path
-        else:
-            print(f"Simulation failed getting results")
             return []
 
 
@@ -156,14 +143,45 @@ class CoinGeckoMonteCarloSimulation:
         graph_html = mpld3.fig_to_html(fig)
         return graph_html
 
+
+    def visualize_history_graph(self):
+        timestamps, prices = self.fetch_price_data(coin_id=self.coin_id, years=self.years, return_timestamps=True)
+        history_graph = pd.DataFrame({"Timestamps": timestamps, "Prices": prices})
+
+        # Plotting using Matplotlib
+        fig, ax = plt.subplots(figsize=(14, 11))
+        ax.plot(history_graph["Timestamps"], history_graph["Prices"])
+        ax.set_xlabel("Timestamps")
+        ax.set_ylabel("Prices in $")
+        ax.grid(True)
+        min_price = history_graph['Prices'].min()
+        max_price = history_graph['Prices'].max()
+        average_price = history_graph['Prices'].mean()
+
+        # Add extra information at the top of the graph
+        extra_info = [
+            f"Cryptocurrency: {self.coin_id.capitalize()}",
+            f"Years of Price Data Collected: {self.years} years",
+            f"Maximum Price: {max_price:.2f}$",
+            f"Minimum Price: {min_price:.2f}$",
+            f"Average price per {self.years} years: {average_price:.2f}$",
+        ]
+
+        # Set the y-coordinate for the text
+        y_coord = 1.1
+
+        # Iterate over each line of text and place it on a different row
+        for line in extra_info:
+            ax.text(0.5, y_coord, line, transform=ax.transAxes, ha='center', va='center', fontsize=14)
+            y_coord -= 0.02  # Adjust this value to control the spacing between rows
+
+        # Convert the Matplotlib figure to HTML using mpld3
+        history_html = mpld3.fig_to_html(fig)
+        plt.close(fig)  # Close the Matplotlib figure to free up resources
+
+        return history_html
+
         
-        
-        
-
-
-
-
-
 if __name__ == "__main__":
     # coin_id = str(input("Enter the cryptocurrency symbol: "))
     # years = int(input("Enter the number of years of historical data to consider: "))
@@ -172,7 +190,8 @@ if __name__ == "__main__":
     # num_simulations = int(input("Enter the number of Monte Carlo simulations: "))
 
     # run_simulation_and_print_results(coin_id, years, principal_amount, investment_horizon, num_simulations)
-    monte_carlo = CoinGeckoMonteCarloSimulation("solana", 1, 1000, 100, 23)
+    monte_carlo = CoinGeckoMonteCarloSimulation("bitcoin", 4, 1000, 100, 23)
     # monte_carlo.visualize_average_future_value()
-    monte_carlo.visualize_simulation()
+    # monte_carlo.visualize_simulation()
+    monte_carlo.visualize_history_graph()
     
