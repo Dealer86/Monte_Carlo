b067168e84c7039f53d2bb3c0a9734aca218284d
Adrian Croitor <adicroitor86@yahoo.com>
2023-11-14 12:59:46 +0200

    Formatted with black.

--- montecarlo/simulation/views.py
+++ montecarlo/simulation/views.py
@@ -5,30 +5,38 @@ from django.contrib import messages
 
 
 def montecarlo(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = MonteCarloForm(request.POST)
-        
+
         if form.is_valid():
             cleaned_data = form.cleaned_data
-            coin_id = cleaned_data['coin_id']
-            years = cleaned_data['years']
-            principal_amount = cleaned_data['principal_amount']
-            investment_horizon = cleaned_data['investment_horizon']
-            num_simulations = cleaned_data['num_simulations']
+            coin_id = cleaned_data["coin_id"]
+            years = cleaned_data["years"]
+            principal_amount = cleaned_data["principal_amount"]
+            investment_horizon = cleaned_data["investment_horizon"]
+            num_simulations = cleaned_data["num_simulations"]
 
             monte_carlo = CoinGeckoMonteCarloSimulation(
-                coin_id, years, principal_amount, investment_horizon, num_simulations)
+                coin_id, years, principal_amount, investment_horizon, num_simulations
+            )
             try:
                 graph_html = monte_carlo.visualize_simulation()
                 history_html = monte_carlo.visualize_history_graph()
             except IndexError:
-                messages.error(request, f"Cryptocurrency symbol {coin_id} is not a valid! Try again ...") 
-                return redirect('montecarlo')
-            
-            return render(request, 'simulation/montecarlo.html', {'form': form, "graph_html": graph_html, 'history_html': history_html})
-        
+                messages.error(
+                    request,
+                    f"Cryptocurrency symbol {coin_id} is not a valid! Try again ...",
+                )
+                return redirect("montecarlo")
+
+            return render(
+                request,
+                "simulation/montecarlo.html",
+                {"form": form, "graph_html": graph_html, "history_html": history_html},
+            )
+
         else:
             messages.error(request, "Fill all tabs before running simulation!")
     else:
         form = MonteCarloForm()
-    return render(request, 'simulation/montecarlo.html', {'form': form})
+    return render(request, "simulation/montecarlo.html", {"form": form})
